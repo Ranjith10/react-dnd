9bd058950c438b4480089c52ef9096e4193439ee
Ranjith kumar <ranjit4072@gmail.com>
2020-09-27 16:29:22 +0530

    Implemented useDrag

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import { useDrag } from 'react-dnd'
 
-const Box = () => {
-    return (
-        <div>
-            <>sample</>
-        </div>
-    )
+const style = {
+  border: '1px dashed gray',
+  backgroundColor: 'white',
+  padding: '0.5rem 1rem',
+  marginRight: '1.5rem',
+  marginBottom: '1.5rem',
+  cursor: 'move',
+  float: 'left',
+}
+const Box = ({ name }) => {
+  const [{ isDragging }, drag] = useDrag({
+    item: { name, type: 'box' },
+    end: (item, monitor) => {
+      const dropResult = monitor.getDropResult()
+      if (item && dropResult) {
+        alert(`You dropped ${item.name} into ${dropResult.name}!`)
+      }
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  })
+  const opacity = isDragging ? 0.4 : 1
+  return (
+    <div ref={drag} style={{ ...style, opacity }}>
+      {name}
+    </div>
+  )
 }
 
 export default Box
--- src/components/Components.js
+++ src/components/Components.js
@@ -10,7 +10,7 @@ const Components = () => {
                 <Dustbin />
             </div>
             <div>
-                <Box />
+                <Box name = 'sample' />
             </div>
         </>
     )
